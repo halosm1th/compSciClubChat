16dd320348fe4ac68c950af0de46abbd867c90ca
thomas <thomastrkie@gmail.com>
2016-05-25 15:32:57 -0400

    Added a setup script and a compile script!

--- ChatAppServer/ChatAppServer/compile.py
+++ ChatAppServer/ChatAppServer/compile.py
@@ -1,3 +1,3 @@
 import os
-
+os.system('echo compiling! To run the server. type go to Debug and type MonochatAppServer.exe')
 os.system('dmcs -r:System.Data.dll -r:Mono.Data.Sqlite *.cs -out:bin/Debug/ChatAppServer.exe')
---
+++ ChatAppServer/ChatAppServer/setup.py
@@ -0,0 +1,4 @@
+import os
+
+os.system('echo Will install needed programs on linux. If on windows please follow the instrctions on githuh')
+os.system('sudo apt-get update && sudo apt-get upgrade && sudo apt-get install sqlite3 mono-complete sl')
